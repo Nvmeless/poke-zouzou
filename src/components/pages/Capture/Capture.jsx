3f679af18dfb94febf99a563d364ff21828e63b1
Jinn <alexandre.Quilan.delaistre@gmail.com>
2023-11-08 19:34:08 +0100

    caputre

--- src/components/pages/Capture/Capture.jsx
+++ src/components/pages/Capture/Capture.jsx
@@ -1,7 +1,40 @@
-import React from "react";
-
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { PokedexEntrie } from "../../molecules";
 const Capture = () => {
-  return <div>Capture</div>;
+  const [datas, setDatas] = useState(false);
+  const [caught, setCaught] = useState("try");
+  const [pokemon, setPokemon] = useState(null);
+  const [pokedex, setPokedex] = useState(null);
+  useEffect(() => {
+    axios
+      .post("http://127.0.0.1:32819/api/pokemon/catch", {})
+      .then(function (response) {
+        setDatas(response?.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, []);
+  useEffect(() => {
+    console.log(datas);
+    if (datas?.message == "not catched") {
+      setCaught(false);
+    } else {
+      setCaught(true);
+      setPokemon(datas);
+      setPokedex(datas.pokedex);
+    }
+  }, [datas]);
+
+  return (
+    <div>
+      <h1>CAPUTRE</h1>
+      {caught === true ? <PokedexEntrie ></PokedexEntrie> : <div>Raté ...</div>}
+
+      {/* <PokedexEntrie></PokedexEntrie> */}
+    </div>
+  );
 };
 
 export default Capture;
